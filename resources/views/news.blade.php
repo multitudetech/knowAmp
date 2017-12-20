2f5f13da39e301576aa95c18db4024fdac1e0a41
tejasambalia <tejas.ambalia1994@gmail.com>
2017-12-21 00:50:24 +0530

    grid changed

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,13 +6,13 @@ $_news = content_news::get();
 ?>
 <div class="container">
 	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="col-md-9 col-sm-9 col-xs-9">
 			<h1>News</h1>
 		</div>
 	</div>
 	<?php foreach ($_news as $d) { ?>
 		<div class="row">
-			<div class="col-md-12 col-sm-12 col-xs-12">
+			<div class="col-md-9 col-sm-9 col-xs-9">
 				<a href="<?php echo $d->url; ?>"><h2><?php echo $d->title; ?></h2></a>
 				<div class="col-md-3 col-sm-12 col-xs-12">
 					<img class="img-responsive" src="<?php echo $d->image_link; ?>">
